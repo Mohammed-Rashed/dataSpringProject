fef189d2fc08c693f2e56085cb34d05fa8f498ef
mohammed-rashed <jstrasshed@gmail.com>
2024-08-31 06:05:57 +0300

    update employee

--- src/main/java/com/dataProject/dataProject/controller/EmployeeController.java
+++ src/main/java/com/dataProject/dataProject/controller/EmployeeController.java
@@ -20,6 +20,10 @@ public class EmployeeController {
     public Employee save(@RequestBody Employee employee){
         return empService.save(employee);
     }
+    @PostMapping("/update")
+    public Employee update(@RequestBody Employee employee){ // diff with store is provide id in request body and jpa update record if found the row with id
+        return empService.save(employee);
+    }
     @GetMapping("/delete")
     public void save(@RequestParam Integer id){
          empService.delete(id);
